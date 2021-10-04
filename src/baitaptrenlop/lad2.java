0d35edc8d13d7127f8b4dd7f50b7633b494a519b
Le-hoang-thach <hoangthachnao@gmail.com>
2021-10-04 11:02:41 +0700

    lên code

---
+++ src/baitaptrenlop/lad2.java
@@ -0,0 +1,16 @@
+package baitaptrenlop;
+
+import java.util.ArrayList;
+
+public class lad2 {
+    public static void main(String[] args) {
+        ArrayList a = new ArrayList();
+        a.add("Cường");
+        a.add(true);
+        a.add(1);
+        a.add(2.5);
+        Integer x = (Integer)a.get(2);
+        System.out.println(a);
+        
+    }
+}
