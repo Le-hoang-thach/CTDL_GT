34674cee7e7de0a13d2eb4005093ac9e0accd11b
Le-hoang-thach <hoangthachnao@gmail.com>
2021-10-11 09:12:21 +0700

    chinh sua

--- src/com/lehoangthach2008110288/tuan01/lad1.java
+++ src/com/lehoangthach2008110288/tuan01/lad1.java
@@ -3,8 +3,9 @@ package com.lehoangthach2008110288.tuan01;
 import java.util.Scanner;
 
 public class lad1 {
+    static Scanner bienNhap = new Scanner(System.in);
     public static void main(String[] args) {
-        Scanner bienNhap = new Scanner(System.in);
+        
         System.out.print("Họ và tên: ");
         String name =  bienNhap.nextLine();
         System.out.print("Điểm Tb: ");
